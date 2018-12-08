bcde94edde339344a2f4ac9bb3ae69ffa02438d6
Michiel Erasmus <swartskaap@gmail.com>
2018-12-08 19:56:46 +0100

    Eerste commit

--- neopixel-rondje-lopen/neopixel-rondje-lopen.js
+++ neopixel-rondje-lopen/neopixel-rondje-lopen.js
@@ -1,6 +1,7 @@
 let range: neopixel.Strip = null
 let startLED1 = 0
 let strip: neopixel.Strip = null
+let aantalLEDs = 0
 let regenBoogteller1 = 0
 function knopAuitnodigen() {
     for (let i = 0; i < 3; i++) {
@@ -12,7 +13,7 @@ function knopAuitnodigen() {
 function doenRondje() {
     strip.clear()
     startLED1 = 0
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < aantalLEDs; i++) {
         range = strip.range(startLED1, 1)
         strip.setPixelColor(startLED1, neopixel.rgb(255, Math.randomRange(50, 200), Math.randomRange(0, 255)))
         strip.show()
@@ -38,22 +39,24 @@ input.onButtonPressed(Button.B, function () {
 function doenRegenboogje1() {
     strip.clear()
     regenBoogteller1 = 0
-    for (let regenBoogteller1 = 0; regenBoogteller1 <= 7; regenBoogteller1++) {
-        basic.pause(100)
+    for (let regenBoogteller1 = 0; regenBoogteller1 <= aantalLEDs; regenBoogteller1++) {
+        basic.pause(500)
         range = strip.range(regenBoogteller1, 2)
         strip.setPixelColor(regenBoogteller1, neopixel.rgb(Math.randomRange(0, 255), Math.randomRange(0, 255), Math.randomRange(0, 255)))
         strip.show()
     }
     for (let i = 0; i < 2; i++) {
-        basic.pause(100)
+        basic.pause(500)
         strip.clear()
         strip.setPixelColor(regenBoogteller1, neopixel.rgb(Math.randomRange(0, 255), Math.randomRange(0, 255), Math.randomRange(0, 255)))
         strip.show()
     }
     basic.pause(100)
-    strip.clear()
 }
-strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
+// Aantal LEDs op neopixel
+//
+aantalLEDs = 8
+strip = neopixel.create(DigitalPin.P0, aantalLEDs, NeoPixelMode.RGB)
 strip.clear()
 strip.setBrightness(50)
 knopAuitnodigen()
